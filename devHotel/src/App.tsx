29a1350ec205e6e8f9f1611df4f680ee80450898
JoséMiguel Marte <tumusicalatina@gmail.com>
2024-06-01 10:43:40 -0400

    refactored

--- devHotel/src/App.tsx
+++ devHotel/src/App.tsx
@@ -1,55 +1,12 @@
-import { useState } from "react";
-import Room from "./components/Room";
+import Room from "./components/Room"
 
-function App() {
-  // const [roomType, setRoomType] = useState("")
-  // Use useState to select a room
-  const [selectedRoom, setSelectedRoom] = useState(null);
-  // State for num of nights
-  const [numNights, setNumNights] = useState(0);
-  // Types of rooms we have
-  const rooms = [
-    {
-      type: "Single",
-      price: 50,
-      beds: 1,
-      baths: 1,
-      description: "A single room with a king size bed and a bathroom.",
-    },
-    {
-      type: "Double",
-      price: 75,
-      beds: 2,
-      baths: 1,
-      description: "A double room with a king size bed and a bathroom.",
-    },
-    {
-      type: "Suite",
-      price: 100,
-      beds: 3,
-      baths: 1,
-      description: "A triple room with a king size bed and a bathroom.",
-    },
-  ]; // End of rooms array
-  const selectARoom = (room) => {
-    console.log("You selected room: ", room);
 
-    setSelectedRoom(room);
-  };
-
+const App = () => {
   return (
-    <div className="hotel">
-      <h1>DevAccelerator Hotel</h1>
-      <h2>Rooms to rent</h2>
-      <div className="rooms">
-        {/* Loop through each room and display it */}
-        {rooms.map((room) => (
-          <Room key={room.type} roomType={room.type} price={room.price} onClick = {() => console.log("test")
-          }/>
-        ))}
-      </div>
-    </div>
-  );
+   <>
+   <Room />
+   </>
+  )
 }
 
-export default App;
+export default App
--- devHotel/src/components/Room.tsx
+++ devHotel/src/components/Room.tsx
@@ -1,20 +1,9 @@
 
 
-const Room = ({ roomType, price}) =>{ // beds, baths, description, image }) => {
-  // const [roomType, setRoomType] = useState("")
-  // const [price, setPrice] = useState("")
-  // const [beds, setBeds] = useState("")
-  // const [baths, setBaths] = useState("")
-  // const [description, setDescription] = useState("")
-  // const [image, setImage] = useState("")
-  // Display room information here
+const Room = () => {
   return (
-    <div className="room">
-        <h2>{roomType}</h2>
-        <p>Price Per Night: {price}</p>
-    
-    </div>
-  );
+    <div>Room</div>
+  )
 }
 
 export default Room

1705323b7d8687337ba3a24c08513374ee779b2c
JoséMiguel Marte <tumusicalatina@gmail.com>
2024-06-01 14:04:17 -0400

    Done?

--- devHotel/src/components/Room.tsx
+++ devHotel/src/components/Room.tsx
@@ -1,9 +1,18 @@
 
-
-const Room = () => {
-  return (
-    <div>Room</div>
-  )
+interface RoomProps {
+  room: { id: number; type: string; price: number };
+  onSelect: () => void;
 }
 
-export default Room
+const Room = ({room, onSelect}: RoomProps) => {
+    return (
+        <div>
+            <h2>Room:{room.type} </h2>
+            <h3>Price: ${room.price}</h3>
+            <button onClick={onSelect}>Select</button>
+        </div>
+    );
+};
+
+
+export default Room;
